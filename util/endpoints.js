1fabb76bc5d6c510d1ea17e9ff1c95f2c753f8c8
weiguo.ni <neefrankie@gmail.com>
2018-11-27 23:35:26 +0800

    api url

---
+++ util/endpoints.js
@@ -0,0 +1,7 @@
+const baseUrl = "http://localhost:8200";
+
+module.exports = {
+  "wxOrder": `${baseUrl}/unified-order`,
+  "aliOrder": `${baseUrl}/app-order`,
+  "plans": `${baseUrl}/paywall/plans`,
+};
