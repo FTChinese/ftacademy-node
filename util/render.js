54d32933f785446a2ac8de365abca136cf4cf393
weiguo.ni <neefrankie@gmail.com>
2018-11-27 23:35:41 +0800

    add filter toCurrency

--- util/render.js
+++ util/render.js
@@ -1,15 +1,24 @@
-const path = require('path');
-const nunjucks = require('nunjucks');
-const util = require('util');
-nunjucks.configure(
+const path = require("path");
+const nunjucks = require("nunjucks");
+const util = require("util");
+const numeral = require("numeral")
+
+const env = nunjucks.configure(
   [
-    path.resolve(__dirname, '../view'),
-    path.resolve(__dirname, '../client')
+    path.resolve(__dirname, "../view"),
+    path.resolve(__dirname, "../client")
   ], 
   {
-    noCache: process.env.NODE_ENV === 'development',
-    watch: process.env.NODE_ENV === 'development'
+    noCache: process.env.NODE_ENV === "development",
+    watch: process.env.NODE_ENV === "development"
   }
 );
+
+/**
+ * Conert a number to currency string.
+ */
+env.addFilter("toCurrency", function(num) {
+  return numeral(num).format("0,0.00");
+});
 
 module.exports = util.promisify(nunjucks.render);
