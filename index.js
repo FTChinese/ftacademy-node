46e8c594058e24d28f313cf1b730209521fb801e
weiguo.ni <neefrankie@gmail.com>
2018-12-03 18:00:38 +0800

    add middlewares

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const session = require("koa-session");
 const boot = require("./util/boot-app");
 
 const env = require("./middleware/env");
+const handlError = require("./middleware/handle-error");
+const noCache = require("./middleware/no-cache");
 
 const home = require("./server/home");
 const subscription = require("./server/subscription");
@@ -19,20 +21,29 @@ const router = new Router();
 
 app.proxy = true;
 
+app.use(logger());
+
 if (!isProduction) {
   const static = require("koa-static");
   app.use(static(path.resolve(process.cwd(), "node_modules")));
   app.use(static(path.resolve(process.cwd(), "client")));
 }
 
+app.use(session({
+  key: "_ftc:sess",
+  signed: false
+}, app));
 app.use(bodyParser());
 app.use(env());
+app.use(handlError());
 
 router.get("/", home)
 router.use("/subscription", subscription);
 router.use("/__version", version);
 
 app.use(router.routes());
+
+console.log(router.stack.map(layer => layer.path));
 
 boot(app)
   .catch(err => {
