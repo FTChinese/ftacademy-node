4d83ee520650e603e4c014780a4b20215458ce88
weiguo.ni <neefrankie@gmail.com>
2019-02-28 00:03:14 +0800

    redefine types

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,39 @@
-declare interface Banner {
+declare interface IBanner {
     coverUrl: string;
     heading: string;
     subHeading: string;
     content: string[];
 }
 
-declare interface Plan {
+declare interface IPlan {
     tier: "standard" | "premium";
-    cycle: "year" | "month";
-    price: number;
-    id: number;
+    cycle: "month" | "year";
+    listPrice: number;
+    netPrice: number;
     description: string;
-    ignore?: boolean;
+    message?: string;
 }
 
-declare interface Pricing {
-    standard_year: Plan;
-    standard_month: Plan;
-    premium_year: Plan;
+declare interface IPricing {
+    standard_year: IPlan;
+    standard_month: IPlan;
+    premium_year: IPlan;
 }
 
-declare interface Promotion {
+declare interface IPaywall {
+    plans: IPricing;
+    banner: IBanner;
+}
+declare interface IPromo extends IPaywall {
     startAt: string;
     endAt: string;
-    plans: Pricing;
-    banner: Banner;
 }
 
-/**
- * Show original price and sale price side by side.
- */
-declare interface PriceTag {
-    cycle: "year" | "month";
-    original: number;
-    sale?: number;
-}
-
-declare interface Product {
-    tier: "standard" | "premium";
-    cycles: ["year", "month"];
-    prices: PriceTag[];
-    currency: "CNY";
+declare interface IProduct {
     heading: string;
     benefits: string[];
+    tier: "standard" | "premium";
+    currency: string;
+    pricing: IPlan[];
     smallPrint?: string;
 }
