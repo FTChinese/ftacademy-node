8dbdda95a7ec50d809d5810bce341b9e953c3bf6
weiguo.ni <neefrankie@gmail.com>
2019-04-05 16:24:36 +0800

    feat: localization resources

---
+++ model/localized.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "year": "年",
+  "month": "月",
+  "CNY": "¥",
+  "standard": "标准会员",
+  "premium": "高端会员",
+  "tenpay": "微信支付",
+  "alipay": "支付宝",
+  "stripe": "Stripe",
+  "wechat": "微信支付"
+}
