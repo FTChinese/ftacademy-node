c68f724b668a3d14f9420c61ddf06ef8b1af4c19
weiguo.ni <neefrankie@gmail.com>
2019-04-05 16:25:29 +0800

    update: type definition

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,42 @@
 declare interface IBanner {
-    coverUrl: string;
-    heading: string;
-    subHeading: string;
-    content: string[];
-}
-
-declare interface IPlan {
-    tier: "standard" | "premium";
-    cycle: "month" | "year";
-    listPrice: number;
-    netPrice: number;
-    description: string;
-    message?: string;
-}
-
-declare interface IPricing {
-    standard_year: IPlan;
-    standard_month: IPlan;
-    premium_year: IPlan;
-}
-
-declare interface IPaywall {
-    plans: IPricing;
-    banner: IBanner;
-}
-declare interface IPromo extends IPaywall {
-    startAt: string;
-    endAt: string;
+  heading: string;
+  subHeading: string;
+  coverUrl: string;
+  content: string[];
 }
 
 declare interface IProduct {
-    heading: string;
-    benefits: string[];
-    tier: "standard" | "premium";
-    currency: string;
-    pricing: IPlan[];
-    smallPrint?: string;
+  heading: string;
+  benefits: string[];
+  smallPrint?: string;
+  tier: "standard" | "premium";
+  currency: string;
+  pricing: IPlan[];
+}
+
+declare interface IPlan {
+  tier: "standard" | "premium";
+  cycle: "month" | "year";
+  listPrice: number;
+  netPrice: number;
+  description: string;
+}
+
+declare interface IPricing {
+  standard_year: IPlan;
+  standard_month: IPlan;
+  premium_year: IPlan;
+}
+
+declare interface IPaywall {
+  banner: IBanner;
+  products: IProduct[];
+}
+
+declare interface IPromo {
+  startAt: string;
+  endAt: string;
+  banner: IBanner;
+  pricing: IPricing;
+  createdAt: string;
 }
