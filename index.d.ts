cfa917f1f7c43e1b56e1cff862fa197314ea3679
weiguo.ni <neefrankie@gmail.com>
2019-04-07 13:29:58 +0800

    type definition for account and oauth

--- index.d.ts
+++ index.d.ts
@@ -45,3 +45,38 @@ declare interface IOAuthClient {
   client_id: string;
   client_secret: string;
 }
+
+// This is not the standard OAuth token.
+declare interface IOAuthToken {
+  userId: string;
+  loginMethod: "email" | "wechat"
+}
+
+declare interface ICredentials {
+  email: string;
+  password: string;
+}
+
+declare interface IMembership {
+  tier?: "standard" | "premium";
+  cycle?: "year" | "month";
+  expireDate?: string;
+}
+
+declare interface IWechat {
+  nickname?: string;
+  avatarUrl?: string;
+}
+
+declare interface IAccount {
+  id: string;
+  unionId?: string;
+  userName?: string;
+  email: string;
+  isVerified: boolean;
+  avatar?: string;
+  isVip: boolean;
+  loginMethod: "email" | "wechat";
+  wechat: IWechat;
+  membership: IMembership;
+}
