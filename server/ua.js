f6c5d8755030d038c7ee4d8ac0c3719e4f5fd691
weiguo.ni <neefrankie@gmail.com>
2019-04-19 18:29:08 +0800

    fix: user agent testing

--- server/ua.js
+++ server/ua.js
@@ -5,11 +5,10 @@ const router = new Router();
 router.get("/", async (ctx, next) => {
 
   const userAgent = ctx.header["user-agent"];
-  const ua = uaParser(userAgent);
 
   ctx.body = {
     description: "This is a test page to detect user agent header",
-    rawUserAgent: ua,
+    rawUserAgent: userAgent,
   }
 });
 
