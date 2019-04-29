15ead932ca5361ef11c90097cd1f78f8e3bb246d
weiguo.ni <neefrankie@gmail.com>
2019-04-29 17:33:51 +0800

    refactor: handle order query not found

--- server/pay-result.js
+++ server/pay-result.js
@@ -8,6 +8,9 @@ const {
 const {
   checkSession,
 } = require("./middleware");
+const {
+  ClientError,
+} = require("../lib/response");
 
 const router = new Router();
 
@@ -23,10 +26,14 @@ router.get("/ali/done",
      * @type {ISubsOrder}
      */
     const subsOrder = ctx.session.subs;
+
+    debug("Sub order from session: %O", subsOrder);
+
     /**
      * @type {string}
      */
     const fromUrl = ctx.session.from;
+    debug("From url: %s", fromUrl);
 
     /**
      * @type {Account}
@@ -62,9 +69,13 @@ router.get("/wx/done",
      * @type {ISubsOrder}
      */
     const subsOrder = ctx.session.subs;
+
+    debug("Subs order from session: %O", subsOrder);
+
     // If users jumped to this site from somewhere else, like in the middle of reading an article,
     // redirec them back after payment finished.
     const fromUrl = ctx.session.from;
+    debug("From url: %s", fromUrl);
 
     // Show redirect URL.
     ctx.state.redirectTo = fromUrl
@@ -74,22 +85,39 @@ router.get("/wx/done",
     /**
      * @type {Account}
      */
-    const account = ctx.session.user;
+    const account = ctx.state.user;
 
-    if (subsOrder) {
-      const payResult = await account.wxOrderQuery(subsOrder.orderId);
+    try {
+      if (subsOrder) {
+        const payResult = await account.wxOrderQuery(subsOrder.orderId);
 
-      ctx.state.result = payResult;
+        ctx.state.result = payResult;
+      }
+    } catch (e) {
+      const clientErr = new ClientError(e);
+
+      if (!clientErr.isFromAPI()) {
+        throw e;
+      }
+
+      switch (e.status) {
+        case 404:
+          break;
+
+        default:
+          ctx.state.errors = clientErr.buildGenericError();
+          break;
+      }
     }
 
-    ctx.state.result = {
-      "paymentState": "SUCCESS",
-      "paymentStateDesc": "支付成功",
-      "totalFee": 1,
-      "transactionId": "4200000252201903069440709666",
-      "ftcOrderId": "FT1D3CEDDB2599EFB9",
-      "paidAt": "2019-03-06T07:21:18Z"
-    };
+    // ctx.state.result = {
+    //   "paymentState": "SUCCESS",
+    //   "paymentStateDesc": "支付成功",
+    //   "totalFee": 1,
+    //   "transactionId": "4200000252201903069440709666",
+    //   "ftcOrderId": "FT1D3CEDDB2599EFB9",
+    //   "paidAt": "2019-03-06T07:21:18Z"
+    // };
 
     ctx.body = await render("wxpay-done.html", ctx.state);
 
